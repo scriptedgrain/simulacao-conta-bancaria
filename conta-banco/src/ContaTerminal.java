04f384d9a60f898269c7b561fe9326e0fa55607c
Lucca Maia <luccamairosa@gmail.com>
2024-03-06 17:27:42 -0300

    versão final

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,38 @@
+import java.util.Scanner;
 public class ContaTerminal {
+    private static int numero;
+    private static String agencia;
+    private static String nomeCliente;
+    private static double saldo;
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+    //Inserção de dados pelo usuário:
+
+        Scanner src = new Scanner(System.in);
+
+        System.out.println("\nInforme o número de sua conta: ");
+        numero = src.nextInt();
+        
+        System.out.println("\nInforme o número de sua agência: ");
+        agencia = src.next();
+
+        System.out.println("\nInforme o seu nome: ");
+        nomeCliente = src.next();
+
+        System.out.println("\nInforme o número de sua agência: ");
+        saldo = src.nextDouble();
+
+    //Impressão dos dados do usuário:
+
+        System.out.println("Olá " 
+        + nomeCliente 
+        + ", obrigado por criar uma conta em nosso banco, sua agência é " 
+        + agencia
+        + ", conta "
+        + numero
+        + " e seu saldo "
+        + saldo
+        + " já está disponível para saque.");
     }
 }
